464ec146648b21272f5da8077e549f728236bcf3
Emerson Jacobson <emwjacobson@gmail.com>
2016-11-28 21:04:46 -0800

    Added search function. Began work for Utterances and Intents.

--- Wolfram.py
+++ Wolfram.py
@@ -1,6 +1,27 @@
 import wolframalpha
-from variables import app_id
+from flask import Flask, render_template
+from flask_ask import Ask, statement
+from variables import *
 
-print(app_id)
+client = wolframalpha.Client(WOLFRAMALPHA_API_KEY)
 
-client = wolframalpha.Client(app_id)
+app = Flask(__name__)
+ask = Ask(app, '/wolfram')
+
+@ask.launch
+def launch():
+    speech_text = "What would you like me to lookup?"
+    reprompt_text = "I can search for almost anything on Wolfram Alpha, what would you like me to look for?"
+    return question(speech_text).reprompt(reprompt_text).simple_card('WolframAlpha', speech_text)
+
+@ask.intent('Search', mapping={'query': 'Query'})
+def search(query):
+    print(query)
+    response = client.query(query)
+    try:
+        response = response['pod'][1]['subpod']['plaintext']
+    except:
+        response = "Unable to find answer!"
+    print(response)
+
+app.run()
